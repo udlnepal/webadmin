6fe6dd0b82c53782b3a261020386039eb300dde1
Sumit <timusumit@gmail.com>
2018-08-14 14:14:27 +0545

    function display made

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -10,24 +10,35 @@ class Site extends CI_Controller {
        $this->load->model('slider_setup_model');
         $this->load->helper(array('form', 'url'));
         $this->load->library(array('session', 'form_validation'));
- 
+    }
+
+    public function display($view,$data){
+    	$this->load->view('includes/header',$data);
+		$this->load->view('includes/navigation',$data);
+		$this->load->view($view,$data);
+		$this->load->view('includes/footer');
+
     }
 
 
 public function index()
 	{
-		
 		$data['header_title_setup'] = $this->header_setup_model->get_header_title();
 		$data['slider_setup'] = $this->slider_setup_model->get_slider_image_name();
 		$data['header_left_menu']=$this->header_setup_model->get_header_left_menu();
-
+		$this->display('site/home',$data);
+		
+/*
 
 	//	print_r ($menudata); exit;
 		$this->load->view('includes/header',$data);
 		$this->load->view('includes/navigation',$data);
 	//	$this->load->view('templates/sidebar');
 		$this->load->view('site/home');
-		$this->load->view('includes/footer');
+		$this->load->view('includes/footer');*/
 	}
+
+
+
 
 }
